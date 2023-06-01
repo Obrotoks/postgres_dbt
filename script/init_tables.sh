1b6f0cfa4bb4f58c82e3f09e78e33c4d6639f151
Obrotoks <110456512+Obrotoks@users.noreply.github.com>
2023-06-01 14:49:47 +0200

    Run DBT w/ Docker

--- script/init_tables.sh
+++ script/init_tables.sh
@@ -5,5 +5,17 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     
-ALTER ROLE ps with password 'ps_2023';
+CREATE TABLE 
+    newClients (
+         Num_Age INT
+        ,Des_EmployeType VARCHAR(100)
+        ,Is_Graduate VARCHAR(3)
+        ,Imp_AnnualIncome INT
+        ,Num_FamilyMembers INT
+        ,Num_ChronicDisease INT
+        ,Is_FrequentFlyer VARCHAR(3)
+        ,Is_EverTravelledAbroad VARCHAR(3)
+    );
+
+COPY newClients FROM '/home/src/Travel Company New Clients.csv' DELIMITER ';' CSV HEADER;
 EOSQL
